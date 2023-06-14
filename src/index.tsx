bc6290c154fdd06c0427396dfe6f260b6911e6bc
Giang Nguyen <giang.nguyen.dev@gmail.com>
2023-06-15 00:50:33 +0700

    feat: add sw

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import React from "react";
 import "./index.css";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { prev_commit } from "./version";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const container = document.getElementById("root");
 const root = createRoot(container as any);
@@ -13,3 +15,18 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+serviceWorkerRegistration.register({
+  onUpdate: (e) => {
+    const { waiting: { postMessage = null } = {} as any, update } = e || {};
+    if (postMessage) {
+      postMessage({ type: "SKIP_WAITING" });
+    }
+    update().then(() => {
+      window.location.reload();
+    });
+  },
+});
+
+// eslint-disable-next-line no-console
+console.log(`App version: ${prev_commit}`);
